036ffd2227a4ece6265cfa01a2460d867df2866d
Hamidur Rahman <Hamidur0405@gmail.com>
2020-04-17 02:21:31 -0400

    setting up first test class

---
+++ src/test/java/EmailValidatorTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@DisplayName("Testing - EmailValidator")
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+public class EmailValidatorTest
+{
+    @Test
+    @DisplayName("email tester")
+    void testEmail()
+    {
+        // TBI
+    }
+}
