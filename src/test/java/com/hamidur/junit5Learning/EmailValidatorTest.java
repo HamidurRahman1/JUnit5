7ea73a8c79af48b49d7e995d7214168d22e6a027
Hamidur Rahman <Hamidur0405@gmail.com>
2020-04-29 01:26:08 -0400

    adding beforeeach and class level instance

--- src/test/java/com/hamidur/junit5Learning/EmailValidatorTest.java
+++ src/test/java/com/hamidur/junit5Learning/EmailValidatorTest.java
@@ -3,26 +3,36 @@ package com.hamidur.junit5Learning;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("Testing - EmailValidator")
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class EmailValidatorTest
 {
+    EmailValidator emailValidator;
+
     @BeforeAll
     static void init()
     {
         System.out.println("initializing some resources before running anything from this class.");
     }
 
+    @BeforeEach
+    void initEach()
+    {
+        emailValidator = new EmailValidator();
+    }
+
     @Test
     @DisplayName("email tester")
     void testEmail()
     {
-        EmailValidator emailValidator = new EmailValidator();
-
         String emailToTest = "random@email.com";
 
         boolean actual = emailValidator.isValid(emailToTest);
@@ -33,8 +43,6 @@ public class EmailValidatorTest
     @Test
     void testEmail2()
     {
-        EmailValidator emailValidator = new EmailValidator();
-
         String emailToTest = "+InvalidName@~invalidDomain.com";
 
         Assertions.assertFalse(emailValidator.isValid(emailToTest),"Email should have been validated to pass the test.");
