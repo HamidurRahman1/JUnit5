a31a9f2f13ffbd81f740060c074dbeac33e50670
Hamidur Rahman <Hamidur0405@gmail.com>
2020-05-08 15:37:20 -0400

    adding MathUtil testAdd()

--- src/test/java/com/hamidur/junit5Learning/MathUtilTest.java
+++ src/test/java/com/hamidur/junit5Learning/MathUtilTest.java
@@ -1,9 +1,11 @@
 package com.hamidur.junit5Learning;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @Tag("mathUtil")
@@ -17,6 +19,15 @@ public class MathUtilTest
     void init()
     {
         mathUtil = new MathUtil();
+    }
+
+    @Test
+    void testAdd()
+    {
+        int a = 1;
+        int b = 1;
+        int expected = a+b;
+        Assertions.assertEquals(expected, mathUtil.add(a, b), () -> "Method should add two numbers.");
     }
 
     @AfterEach
