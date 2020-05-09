aee69c1cdda0bd5953996e5170a8e3563d404775
Hamidur Rahman <Hamidur0405@gmail.com>
2020-05-09 00:23:32 -0400

    Randomized RepeatedTest

--- src/test/java/com/hamidur/junit5Learning/MathUtilTest.java
+++ src/test/java/com/hamidur/junit5Learning/MathUtilTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+
+import java.util.Random;
 
 @Tag("mathUtil")
 @DisplayName("MathUtilTest")
@@ -27,7 +31,21 @@ public class MathUtilTest
         int a = 1;
         int b = 1;
         int expected = a+b;
-        Assertions.assertEquals(expected, mathUtil.add(a, b), () -> "Method should add two numbers.");
+        Assertions.assertEquals(expected, mathUtil.add(a, b), () -> "should add two numbers.");
+    }
+
+    @RepeatedTest(5)
+    @DisplayName("Randomized multiply() test")
+    void testMultiply(RepetitionInfo repetitionInfo)
+    {
+        // use repetitionInfo if conditional repetition is used
+
+        Random random = new Random();
+        int a = random.nextInt(50);
+        int b = random.nextInt(100);
+        int expected = a*b;
+
+        Assertions.assertEquals(expected, mathUtil.multiply(a, b), () -> "should multiply two numbers.");
     }
 
     @AfterEach
